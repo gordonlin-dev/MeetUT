4c65ca2194e705fc754693cd07624ade2faa0335
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-07-19 00:56:15 -0400

    created landing page for app

---
+++ MeetUT_Client/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  Image,
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const logo = require('./logo.png');
+
+const LandingPage = () => {
+  return (
+    <View style={styles.landingPage}>
+      <Image source={logo} style={styles.logo} resizeMode={'contain'} />
+      <View style={styles.buttonContainer}>
+        <Button title="Sign up" />
+        <Button title="Log in" />
+      </View>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={styles.landingPage}>
+      <LandingPage />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  logo: {
+    flex: 10,
+    width: null,
+    height: null,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  landingPage: {
+    flex: 1,
+  },
+});
+
+export default App;
