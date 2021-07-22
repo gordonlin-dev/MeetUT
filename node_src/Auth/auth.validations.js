91a6b879d5ba2e97f53bed47dc17555f68ac927d
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-07-22 00:30:18 -0400

    fixes to /auth

--- node_src/Auth/auth.validations.js
+++ node_src/Auth/auth.validations.js
@@ -9,6 +9,7 @@ exports.passwordMatch = (req, res, next) =>{
             console.log(user)
             let parts = user.password.split('$')
             let salt = parts[0]
+            let passwordHash = parts[1]
             let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest('base64')
             if (passwordHash === hash){
                 return next()
