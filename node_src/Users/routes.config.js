47ae44bd5036e76ee9800a37fa3bdd7ca9ad1deb
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-07-22 22:45:07 -0400

    removed old folder

--- node_src/Users/routes.config.js
+++
@@ -1,7 +0,0 @@
-const UserController = require('./user.controller')
-const AuthValidation = require('../Auth/auth.validations')
-
-exports.routesConfig = function(app){
-    app.post('/users/create', [UserController.createUser])
-    app.get('/users/:userId', [AuthValidation.jwtValid,UserController.getById])
-}
