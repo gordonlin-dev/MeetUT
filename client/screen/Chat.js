56583be8c67046ee655c6e449c1d39930fc91573
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-07-23 22:32:29 -0400

    changes

--- client/screen/Chat.js
+++ client/screen/Chat.js
@@ -5,13 +5,9 @@ const secureStore = require('../SecureStore')
 
 const ChatScreen = props => {
     const connectToChat = () =>{
-        //const socket = io.connect("https://meet-ut-3.herokuapp.com/")
-        //socket.on("connection", msg => {
-        //    console.log('socket connected')
-        //});
         const socket = socketClient("https://meet-ut-3.herokuapp.com/")
         socket.on('connection', () => {
-            console.log(`I'm connected with the back-end`)
+            console.log(`connected`)
         });
     }
 
