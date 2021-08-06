509ee58e2590dcf93ef29b9ede6a26fb22847168
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-08-06 00:32:49 -0400

    fixes

--- backend_chat/chat.model.js
+++ backend_chat/chat.model.js
@@ -80,7 +80,7 @@ exports.createChatRoom = async (participants) => {
 }
 
 exports.getChatRooms = async (userID) => {
-    let user = await this.findOrCreateUsers([userID])
+    let user = await this.findOrCreateUsers([userID])[0]
     for (let i = 0; i < user.chatRooms.length; i++){
         user.chatRooms[i].participants = user.chatRooms[i].participants.filter((value) => {return value !== userID})
     }
