9ab7f9e7f34f9303b70f434284974de2fdf48435
Justin Chan <csnow.to@gmail.com>
2021-08-20 23:40:14 +0800

    login alert

--- client/screen/Authentication/Login.js
+++ client/screen/Authentication/Login.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react'
 import {View, Text, StyleSheet, TextInput, Dimensions, ImageBackground, TouchableOpacity, Alert} from 'react-native'
+
 const secureStore = require('../../SecureStore')
 const cfg = require('../cfg.json')
-const image =  require('../../assets/bg.png');
+const image = require('../../assets/bg.png');
 const {height, width} = Dimensions.get('window');
 const loginSubmit = async (email, password, props) => {
     try {
         const url = cfg.domain + cfg.login;
         const response = await fetch(url, {
-            method : 'POST',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -19,13 +20,14 @@ const loginSubmit = async (email, password, props) => {
         });
         const responseJson = await response.json();
         await secureStore.Save('UserId', email);
-        await secureStore.Save('JWT',responseJson.accessToken);
+        await secureStore.Save('JWT', responseJson.accessToken);
         await secureStore.Save('RefreshToken', responseJson.refreshToken);
         props.navigation.navigate({
-          routeName: 'Home'
+            routeName: 'Home'
         })
-    }catch (error){
+    } catch (error) {
         console.log(error)
+        Alert.alert(cfg.internalError)
     }
 }
 
@@ -35,93 +37,93 @@ const LoginScreen = props => {
 
     return (
         <View style={styles.bg}>
-          <ImageBackground source={image} resizeMode="cover" style={styles.image} >
-          <View>
-              <Text style={styles.header} >
-                  Login
-              </Text>
+            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+                <View>
+                    <Text style={styles.header}>
+                        Login
+                    </Text>
 
-          <TextInput
-              style={styles.Input}
-              onChangeText={onChangeEmail}
-              value={email}
-              placeholder="email"
-              placeholderTextColor="white"
-            />
-            <TextInput
-              style={styles.Input}
-              onChangeText={onChangePassword}
-              value={password}
-              secureTextEntry={true}
-              placeholder="password"
-              placeholderTextColor="white"
-            />
-          <TouchableOpacity
-              onPress={() => {
-                loginSubmit(email, password, props)
-            }}
-              style={styles.Button}>
-              <Text style={styles.font}>Login</Text>
-            </TouchableOpacity>
-          </View>
-        <View>
-          <TouchableOpacity
-              onPress={() =>{
-                  props.navigation.navigate({
-                      routeName: 'ResetPassword'
-                  })
-              }}
-              style={styles.Button}>
-              <Text style={styles.font}>Reset Password</Text>
-            </TouchableOpacity>
-          </View>
+                    <TextInput
+                        style={styles.Input}
+                        onChangeText={onChangeEmail}
+                        value={email}
+                        placeholder="email"
+                        placeholderTextColor="white"
+                    />
+                    <TextInput
+                        style={styles.Input}
+                        onChangeText={onChangePassword}
+                        value={password}
+                        secureTextEntry={true}
+                        placeholder="password"
+                        placeholderTextColor="white"
+                    />
+                    <TouchableOpacity
+                        onPress={() => {
+                            loginSubmit(email, password, props)
+                        }}
+                        style={styles.Button}>
+                        <Text style={styles.font}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            props.navigation.navigate({
+                                routeName: 'ResetPassword'
+                            })
+                        }}
+                        style={styles.Button}>
+                        <Text style={styles.font}>Reset Password</Text>
+                    </TouchableOpacity>
+                </View>
 
-          </ImageBackground>
+            </ImageBackground>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     bg: {
-      flex: 1,
+        flex: 1,
     },
     image: {
-      flex: 1,
-      justifyContent: "center"
+        flex: 1,
+        justifyContent: "center"
     },
     Input: {
-      marginTop: height * 0.03,
-      marginLeft: width * 0.15,
-      height: height * 0.06,
-      width: width * 0.7,
-      borderRadius: 5,
-      borderWidth: 2,
-      padding: 10,
-      borderColor: "white",
-      color: "white"
+        marginTop: height * 0.03,
+        marginLeft: width * 0.15,
+        height: height * 0.06,
+        width: width * 0.7,
+        borderRadius: 5,
+        borderWidth: 2,
+        padding: 10,
+        borderColor: "white",
+        color: "white"
     },
     Button: {
-      width: width * 0.6,
-      height: height * 0.06,
-      marginTop: height * 0.04,
-      marginLeft: width * 0.2,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 15,
-      backgroundColor: 'white',
+        width: width * 0.6,
+        height: height * 0.06,
+        marginTop: height * 0.04,
+        marginLeft: width * 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15,
+        backgroundColor: 'white',
     },
     header: {
-      fontSize:50,
-      marginLeft: width * 0.34,
-      color: "white",
-      fontFamily: 'timeburner',
+        fontSize: 50,
+        marginLeft: width * 0.34,
+        color: "white",
+        fontFamily: 'timeburner',
     },
     font: {
-      fontFamily: 'timeburner',
-      fontSize:18,
-      color: "#0E0EA1",
-      fontWeight: "500"
+        fontFamily: 'timeburner',
+        fontSize: 18,
+        color: "#0E0EA1",
+        fontWeight: "500"
     }
-  });
+});
 
 export default LoginScreen;
