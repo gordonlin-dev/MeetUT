8e2a7dda218bd1979059ee9db6d88f2dc1f6ee21
Justin Chan <csnow.to@gmail.com>
2021-08-24 02:19:49 +0800

    fixed bugs in user model, email verification now checks if user exists

--- backend_user/Users/routes.config.js
+++ backend_user/Users/routes.config.js
@@ -3,8 +3,8 @@ const AuthValidation = require('../Auth/auth.validations')
 
 exports.routesConfig = function(app){
     app.post('/users/create', [UserController.validateEmail, UserController.validatePassword, UserController.confirmPassword, UserController.createUser])
-    app.get('/users/:userID', [AuthValidation.jwtValid,UserController.getById])
-    app.delete('/users/delete/:userID', [AuthValidation.jwtValid,UserController.deleteUser])
+    app.get('/users/:userID', [AuthValidation.jwtValid, UserController.getById])
+    app.delete('/users/delete/:userID', [UserController.deleteUser]) // TODO: Include AuthValidation.jwtValid before deleting
     app.put('/users/updatePassword/:userID', [UserController.validatePassword, UserController.confirmPassword, UserController.updatePassword])
 
 }
