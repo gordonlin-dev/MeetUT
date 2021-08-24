af5266f0064dda4e73c2d53a55166d6eb0097b2c
Justin Chan <csnow.to@gmail.com>
2021-08-24 14:33:01 +0800

    Route todo include AuthValidation.

--- backend_user/Users/routes.config.js
+++ backend_user/Users/routes.config.js
@@ -3,7 +3,7 @@ const AuthValidation = require('../Auth/auth.validations')
 
 exports.routesConfig = function(app){
     app.post('/users/create', [UserController.validateEmail, UserController.validatePassword, UserController.confirmPassword, UserController.sendVerification, UserController.createUser])
-    app.post('/users/verify', [UserController.verifyEmail])
+    app.post('/users/verify', [UserController.verifyEmail]) // TODO: Include AuthValidation.jwtValid
     app.get('/users/:userID', [UserController.getById]) // TODO: Include AuthValidation.jwtValid before deleting
     app.delete('/users/delete/:userID', [UserController.deleteUser]) // TODO: Include AuthValidation.jwtValid before deleting
     app.put('/users/updatePassword/:userID', [UserController.validatePassword, UserController.confirmPassword, UserController.updatePassword])
