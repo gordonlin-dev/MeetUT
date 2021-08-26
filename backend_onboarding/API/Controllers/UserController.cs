e9438d0d2e7f10c5a4c257ee616eb7514061f6bc
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-08-26 00:20:26 -0400

    fixes for user recommendation endpoint

--- backend_onboarding/API/Controllers/UserController.cs
+++ backend_onboarding/API/Controllers/UserController.cs
@@ -29,38 +29,81 @@ namespace API.Controllers
             return "1234";
         }
 
-        [HttpGet]
+        private List<QuestionnaireHobby> GetUserHobbies(User user)
+        {
+            return _context.UserHobbies.Where(x => x.UserId == user.Id).Join(
+                    _context.QuestionnaireHobbies,
+                    userHobby => userHobby.HobbyId,
+                    hobby => hobby.Id,
+                    (userHobby, hobby) => hobby
+                ).ToList();
+        }
+
+        private List<QuestionnaireProgramOfStudy> GetUserPrograms(User user)
+        {
+            return _context.UserProgramOfStudies.Where(x => x.UserId == user.Id).Join(
+                    _context.QuestionnaireProgramOfStudies,
+                    userProgram => userProgram.ProgramId,
+                    program => program.Id,
+                    (userProgram, program) => program
+                ).ToList();
+        }
+
+
+        [HttpPost]
         [Route("Recommendations")]
         public JsonResult GetUserRecommendations(UserRecommendationInput input)
         {
-            var curUserQuery = _context.Users.Where(x => x.Email == input.curUser);
+            var curUserQuery = _context.Users.Where(x => x.Email == input.CurUser);
 
             if (!curUserQuery.Any())
             {
-                return null;
+                return new JsonResult("");
             }
 
             var curUser = curUserQuery.First();
             var userQuery = _context.Users.Where(x => x.Email != curUser.Email).ToList();
-            var matchedUserQuery = _context.Users.Join(
-                    input.matchedUsers,
+            if (input.ExcludedUsers.Any())
+            {
+                var matchedUserQuery = _context.Users.ToList().Join(
+                    input.ExcludedUsers,
                     user => user.Email,
                     matchedUser => matchedUser,
                     (user, matchedUser) => user
                 ).ToList();
-            userQuery = userQuery.Except(matchedUserQuery).ToList();
+                userQuery = userQuery.Except(matchedUserQuery).ToList();
+            }
             userQuery.OrderBy(user => Util.CalculateDistance(user.MatchCoordinates, curUser.MatchCoordinates));
-            return null;
+            var result = new List<RecommendationResult>();
+            foreach(var user in userQuery.Take(100))
+            {
+                var recommendationResult = new RecommendationResult();
+                recommendationResult.UserId = user.Email;
+                recommendationResult.Hobbies = GetUserHobbies(user);
+                recommendationResult.Programs = GetUserPrograms(user);
+                result.Add(recommendationResult);
+            }
+            return new JsonResult(result);
         }
 
         
 
     }
 
+    public class RecommendationResult
+    {
+        public string UserId { get; set; }
+        public List<QuestionnaireHobby> Hobbies { get; set; }
+        public List<QuestionnaireProgramOfStudy> Programs { get; set; }
+        public RecommendationResult()
+        {
+
+        }
+    }
     public class UserRecommendationInput
     {
-        public string curUser { get; set; }
-        public List<string> matchedUsers { get; set; }
+        public string CurUser { get; set; }
+        public List<string> ExcludedUsers { get; set; }
         public UserRecommendationInput()
         {
 
