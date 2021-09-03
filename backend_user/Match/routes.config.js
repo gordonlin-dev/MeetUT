e10859738fd129724888e350bc4dfde9bae8eee9
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-09-02 23:50:01 -0400

    fixed build issues

--- backend_user/Match/routes.config.js
+++ backend_user/Match/routes.config.js
@@ -1,6 +1,5 @@
 const MatchController = require('./match.controller')
 
 exports.routesConfig = function(app){
-    app.get('/match/', [MatchController.getMatchList])
     app.post('/match/like', [MatchController.like])
 }
--- backend_user/app.js
+++ backend_user/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 
 const UserRouter = require('./Users/routes.config')
 const AuthRouter = require('./Auth/routes.config')
-const AlgorithmRouter = require('./Algorithm/routes.config')
 const MatchRouter = require('./Match/routes.config')
 
 app.use(function (req, res, next) {
@@ -24,7 +23,6 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json())
 UserRouter.routesConfig(app)
 AuthRouter.routesConfig(app)
-AlgorithmRouter.routesConfig(app)
 MatchRouter.routesConfig(app)
 
 // start the server listening for requests
