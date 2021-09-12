c60c77f9f9c9b1220fd14324620b1aebc83b0908
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-09-11 23:24:12 -0400

    fixed issue with backend_chat

--- backend_chat/routes.config.js
+++ backend_chat/routes.config.js
@@ -2,7 +2,7 @@ const ChatController = require('./chat.controller')
 const AuthValidation = require('./auth.validation')
 
 exports.routesConfig = function(app){
-    app.post('/chat/user', [AuthValidation.jwtValid, ChatController.createUser()])
+    app.post('/chat/user', [AuthValidation.jwtValid, ChatController.createUser])
     app.post('/chat/create', [AuthValidation.jwtValid,ChatController.createChat])
     app.get('/chat', [AuthValidation.jwtValid,ChatController.getChatRooms])
     app.get('/chat/room/:roomID', [AuthValidation.jwtValid,ChatController.getChatRoom])
