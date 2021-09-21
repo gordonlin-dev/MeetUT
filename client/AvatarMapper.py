6824677beb7efae17294b3fd2e9c027cee38547f
Justin Chan <csnow.to@gmail.com>
2021-09-20 22:18:15 -0400

    Create function to get avatar based on id

--- client/AvatarMapper.py
+++ client/AvatarMapper.py
@@ -4,8 +4,8 @@ import json
 if __name__ == "__main__":
     avatar_map = {}
     avatar_id = 0
-    for file in os.listdir("../../assets/Avatars/"):
-        avatar_map[str(avatar_id)] = "../../assets/Avatars/" + file
+    for file in os.listdir("./assets/Avatars/"):
+        avatar_map[str(avatar_id)] = "./assets/Avatars/" + file
         avatar_id += 1
     with open('./Avatars.json', 'w', encoding='utf-8') as file:
         json = json.dump(avatar_map, file, indent = 4)
