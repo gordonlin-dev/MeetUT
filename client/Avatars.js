24e4148cc20bf5b1912ea73bca0678858004eed1
Justin Chan <csnow.to@gmail.com>
2021-09-23 01:02:36 -0400

    Added remaining icons and removed mapper

--- client/Avatars.js
+++ client/Avatars.js
@@ -1,6 +1,11 @@
 const avatars = [
     { id: 1, source: require("./assets/Avatars/apple-cartoon.png")},
-    { id: 2, source: require("./assets/Avatars/strawberry-cartoon.jpeg")}
+    { id: 2, source: require("./assets/Avatars/avocado-cartoon.jpeg")},
+    { id: 3, source: require("./assets/Avatars/grape-cartoon.jpg")},
+    { id: 4, source: require("./assets/Avatars/lemon-cartoon.jpeg")},
+    { id: 5, source: require("./assets/Avatars/orange-cartoon.png")},
+    { id: 6, source: require("./assets/Avatars/pear-cartoon.jpeg")},
+    { id: 7, source: require("./assets/Avatars/strawberry-cartoon.jpeg")}
 ]
 
 export default avatars
