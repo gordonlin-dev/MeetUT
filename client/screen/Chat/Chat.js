17ce0d592759e259da511339b34047719606e8dc
Gordon.L <gordon.lin@mail.utoronto.ca>
2021-11-16 22:45:52 -0500

    chat page style updates

--- client/screen/Chat/Chat.js
+++ client/screen/Chat/Chat.js
@@ -1,8 +1,18 @@
-import React, {useState, useEffect, useCallback} from 'react'
-import {Image, StyleSheet, Dimensions, BackHandler, View, Text, TouchableWithoutFeedback} from 'react-native'
+import React, {useState, useEffect, useCallback, Fragment} from 'react'
+import {
+    Image,
+    StyleSheet,
+    Dimensions,
+    BackHandler,
+    View,
+    Text,
+    TouchableWithoutFeedback,
+    TouchableOpacity
+} from 'react-native'
 import socketClient  from "socket.io-client";
-import {Bubble, GiftedChat, Message} from 'react-native-gifted-chat';
+import {Bubble, GiftedChat, Message, MessageText, Time} from 'react-native-gifted-chat';
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {provisioningProfileSchema} from "expo-cli/build/credentials/credentials";
 const texts = require("../../assets/Texts.json");
 const handler = require('../Handler')
 const endpoints = require('../../API_endpoints.json')
@@ -14,6 +24,7 @@ const example_profilepic =  require('../../assets/logo.png')
 const ChatScreen = props => {
 
     const [messages, setMessages] = useState([]);
+    const [forceUpdate, setForceUpdate] = useState(false);
     const roomID = props.navigation.state.params.chatRoom;
 
     const sendMessage = async (message) => {
@@ -74,43 +85,39 @@ const ChatScreen = props => {
 
     const renderBubble = props => {
         return(
-            <Bubble
-                {...props}
-                textStyle={{
-                    //right: {
-                    //    color: 'yellow',
-                    //},
-                }}
-                wrapperStyle={{
-                    left: {
-                        backgroundColor: 'white',
-                        marginTop:height*0.01
-                    },
-                }}
-            />
-        )
-    }
-    const renderMessage = props => {
-        console.log(props.currentMessage)
-        const d1 = new Date(props.currentMessage.createdAt)
-        console.log(d1)
-        console.log(new Date(d1.getUTCDate()))
-        return(
             <View>
-                <TouchableWithoutFeedback>
-                    <Message
-                        {...props}
-                        containerStyle={{
-                            left:{
-                                flexDirection:"row",
-                                alignItems:"center"
-                            }
-                        }}
-                    />
-                </TouchableWithoutFeedback>
+                <Bubble
+                    {...props}
+                    textStyle={{
+                        //right: {
+                        //    color: 'yellow',
+                        //},
+                    }}
+                    wrapperStyle={{
+                        left: {
+                            backgroundColor: 'white',
+                            marginTop:height*0.01
+                        },
+                    }}
+                />
             </View>
         )
     }
+    const renderMessage = props => {
+        return(
+            <Message
+                {...props}
+                containerStyle={{
+                    left:{
+                        flexDirection:"row",
+                        alignItems:"center"
+                    }
+                }}
+            />
+        )
+
+    }
+
     return (
         <View style={{backgroundColor:"#d2d2d2", flex:1}}>
             <GiftedChat
@@ -119,16 +126,72 @@ const ChatScreen = props => {
                         <Image source={example_profilepic} style={styles.tinyLogo}/>
                     )
                 }}
-                showAvatarForEveryMessage={true}
                 messages={messages}
                 inverted={false}
                 onSend={message => onSend(message)}
                 user={{
                     _id: 1,
                 }}
-                renderTime={() => {}}
-                renderBubble={renderBubble}
+                renderMessageText={props => {
+                    if(props.currentMessage.user._id === 1){
+                        return(
+                            <TouchableOpacity onPress={()=>{
+                                props.currentMessage["showTime"] = !props.currentMessage["showTime"]
+                                setForceUpdate(!forceUpdate)
+                            }}>
+                                <MessageText currentMessage={props.currentMessage} textStyle={{
+                                    left:{
+                                        color:'white'
+                                    }
+                                }}/>
+                            </TouchableOpacity>
+                        )
+                    }else{
+                        return(
+                            <TouchableOpacity onPress={()=>{
+                                props.currentMessage["showTime"] = !props.currentMessage["showTime"]
+                                setForceUpdate(!forceUpdate)
+                            }}>
+                                <MessageText currentMessage={props.currentMessage} textStyle={{
+                                    left:{
+                                        color:'black'
+                                    }
+                                }}/>
+                            </TouchableOpacity>
+                        )
+                    }
+                }}
+                renderTime={props => {
+                    if(props.currentMessage["showTime"]){
+                        if(props.currentMessage.user._id === 1){
+                            return(
+                                <Time currentMessage={props.currentMessage} timeTextStyle={
+                                    {
+                                        left:{
+                                            color:'white'
+                                        }
+                                    }
+                                }/>
+
+                            )
+                        }else{
+                            return(
+                                <Time currentMessage={props.currentMessage} timeTextStyle={
+                                    {
+                                        left:{
+                                            color:'black'
+                                        }
+                                    }
+                                }/>
+                            )
+                        }
+                    }
+                }}
                 renderMessage={renderMessage}
+                showAvatarForEveryMessage={true}
+                shouldUpdateMessage={(props, nextProps) => {
+                    return forceUpdate || !forceUpdate
+                }}
             />
         </View>
     );
