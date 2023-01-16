1cbcef6fd674672960f3e228b74faa215ad16647
Gordon Lin <gordonlin.dev@gmail.com>
2023-01-16 02:02:48 -0500

    Update env.config.js

--- backend_user/env.config.js
+++ backend_user/env.config.js
@@ -2,7 +2,7 @@ const getConnectionString = () => {
     if(process.env.Connection_String){
         return process.env.Connection_String
     }else{
-        return "mongodb+srv://meetUT:meetUT@cluster0.cwotf.mongodb.net/Cluster0?retryWrites=true&w=majority"
+        return ""
     }
 }
 
@@ -10,7 +10,7 @@ const getJWTSecret = () => {
     if(process.env.JWT_Secret){
         return process.env.JWT_Secret
     }else{
-        return "wz36Yu9kywCWib68qQNZ5LVt4bqo8LuBu8PmzvNFFK9uSS67OLQQ37XToT3ZKuR"
+        return ""
     }
 }
 module.exports = {
