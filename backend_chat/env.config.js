cad322f6875fa9644444a41341ba9401bf4c3104
Gordon Lin <gordonlin.dev@gmail.com>
2023-01-16 02:03:32 -0500

    Update env.config.js

--- backend_chat/env.config.js
+++ backend_chat/env.config.js
@@ -2,7 +2,7 @@ const getConnectionString = () => {
     if(process.env.Connection_String){
         return process.env.Connection_String
     }else{
-        return "mongodb+srv://meetUT:meetUT@chat.3mmja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        return ""
     }
 }
 
@@ -10,7 +10,7 @@ const getJWTSecret = () => {
     if(process.env.JWT_Secret){
         return process.env.JWT_Secret
     }else{
-        return "wz36Yu9kywCWib68qQNZ5LVt4bqo8LuBu8PmzvNFFK9uSS67OLQQ37XToT3ZKuR"
+        return ""
     }
 }
 module.exports = {
